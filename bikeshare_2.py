d2ab143fde4be299451c02b4404ea954f547e5cb
Leyla Sardinha <LMammadova@futureads.local>
2019-09-27 12:57:47 -0700

    just a comment to code file

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -5,7 +5,7 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-
+# this is original template provided by udacity
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
